d7923f7bbe16f7a2fab392684f27c6367a8d1f02
Unknown101001 <37468417+Unknown101001@users.noreply.github.com>
2019-08-20 14:45:00 +0200

    Create picbutton.py

---
+++ picbutton.py
@@ -0,0 +1,11 @@
+class PicButton(QAbstractButton):
+    def __init__(self, pixmap, parent=None):
+        super(PicButton, self).__init__(parent)
+        self.pixmap = pixmap
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(event.rect(), self.pixmap)
+
+    def sizeHint(self):
+        return self.pixmap.size()
