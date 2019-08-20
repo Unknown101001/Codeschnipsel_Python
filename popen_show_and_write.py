48f9645a1fb528342847ff5ee2adbdb4cb216836
Unknown101001 <37468417+Unknown101001@users.noreply.github.com>
2019-08-20 11:02:26 +0200

    Create popen_show_and_write.py

---
+++ popen_show_and_write.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+'''
+import os
+import shutil
+import re
+from distutils.dir_util import copy_tree
+'''
+
+def run():
+    '''
+    run
+    :return: output
+    '''
+    output = []
+    strt = "MY_COMMAND_LINE_COMMAND"
+    process = subprocess.Popen(strt, cwd=".",
+                               stdout=subprocess.PIPE, shell=True)  
+    for line in iter(process.stdout.readline, b''):
+        l = line.decode('utf-8')
+        sys.stdout.write(l)
+        output.append(l.rstrip())
+
+    return output
+
+if __name__ == "__main__":
+    a = run()
